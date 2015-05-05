bc06bb227913b81d111f07f98972dc6a4966b04d
Cade Gillem <cpgillem@gmail.com>
2015-05-04 21:47:19 -0400

    Add users to models.

--- endlesstodolist/models.py
+++ endlesstodolist/models.py
@@ -9,21 +9,24 @@ CHOICE_PRIORITY = (
 )
 
 class Tag(models.Model):
-	""" A tag representation linked to one or more tasks. 
-		Tags can be a location, a project, a group of people, or anything 
-		else that would logically group tasks. 
+	""" A tag representation linked to one or more tasks.
+		Tags can be a location, a project, a group of people, or anything
+		else that would logically group tasks.
 	"""
 	name = models.TextField()
-	
+	user = models.ManyToManyField(User)
+
 class Reminder(models.Model):
 	""" A reminder with a time. """
 	time = models.DateTimeField()
-	
+	user = models.ManyToManyField(User)
+
 class Task(models.Model):
 	""" The base object in any task list.
 		This has a description, due date and time, duration, priority, reminders, and tags.
 	"""
 	description = models.CharField(max_length=255)
+	user = models.ManyToManyField(User)
 	due = models.DateTimeField()
 	duration = models.DurationField()
 	priority = models.IntegerField(choices=CHOICE_PRIORITY)
