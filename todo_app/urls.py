dc5c086ad7f971e1b45bfba0d84c615943a6a5c6
Cade Gillem <cpgillem@gmail.com>
2015-05-04 21:47:35 -0400

    Add url mapping to the app.

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
     # url(r'^$', 'todo_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # URL Structure:
+    # / -> Front page
+    # /tasks/[ID] -> Task by ID
+    # /tags/[tag] -> Tag by name
+    # /users/[username] -> User by name
+
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^/', include('endlesstodolist.urls')),
 ]
