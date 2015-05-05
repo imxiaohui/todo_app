960de195120348e794d76c25553392d44ac8960c
Cade Gillem <cpgillem@gmail.com>
2015-05-04 22:18:14 -0400

    Add view for main page.

--- endlesstodolist/views.py
+++ endlesstodolist/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import RequestContext, loader
 
-# Create your views here.
+from .models import Task, Tag, Reminder
+
+def index(request):
+    template = loader.get_template('endlesstodolist/home.html')
+    context = RequestContext(
+        request,
+        {
+            'test_text': "Please Ignore",
+        }
+    )
+    return HttpResponse(template.render(context))
