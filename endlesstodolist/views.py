4e02a31e5687192515e87527cd3c241cd6dbced4
Cade Gillem <cpgillem@gmail.com>
2015-05-07 19:13:29 -0400

    Add skeletal login form and home page. Still needs actual authentication.
    thank mr skeltal

--- endlesstodolist/views.py
+++ endlesstodolist/views.py
@@ -1,24 +1,24 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.template import RequestContext, loader
+
 from django.contrib.auth import authenticate, login
 
 from .models import Task, Tag, Reminder
+from .forms import LoginForm
 
 def index(request):
-    context = {}
-    template = 'endlesstodolist/home.html'
-    # If there is any POST data:
-    if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        # If we have an active user:
-        ## This can later be expanded to show a page when the user's account is inactive.
-        if user is not None and user.is_active:
-            login(request, user)
-            #TODO THIS LINE: get a list of tasks, etc. and put it in context_dict.
-            template = 'endlesstodolist/user_home.html'
-        # If not, show the main page. In the future, this will become an error page.
+    if request.user.is_authenticated():
+        return render(request, 'endlesstodolist/home.html')
+    else:
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                return HttpResponseRedirect('/home/')
+        else:
+            form = LoginForm()
+            return render(request, 'endlesstodolist/login.html', {
+                'form': form
+            })
 
-    return render(request, template, context)
+def home(request):
+    pass
