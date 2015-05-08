353bec6e0680cfa597d66de87a34c42b35b5c335
Cade Gillem <cpgillem@gmail.com>
2015-05-07 22:38:12 -0400

    fix untracked files

--- endlesstodolist/urls.py
+++ endlesstodolist/urls.py
@@ -2,6 +2,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # / -> Home page for tasks
+    # / -> Home page for site
     url(r'^$', views.index, name='index'),
+    # /home/ -> Home page for tasks and control panel
+    url(r'^home/$', views.home, name='home'),
+    # /logout/ -> Logs the user out and redirects to the main page.
+    url(r'^logout/$', views.logout_user, name='logout_user'),
+    # /tasks/* -> Task by ID
+    url(r'^task/(?P<task_id>[0-9]+)/$', views.task, name='task'),
+    # /tags/* -> Tag by name
+    url(r'^tag/(?P<tag_name>$)/$', views.tag, name='tag')
 ]
--- endlesstodolist/views.py
+++ endlesstodolist/views.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 from .models import Task, Tag, Reminder
 from .forms import LoginForm
 
 def index(request):
-    if request.user.is_authenticated():
-        return render(request, 'endlesstodolist/home.html')
-    else:
+    if not request.user.is_authenticated():
+        # Show the login form
         if request.method == 'POST':
             form = LoginForm(request.POST)
             if form.is_valid():
@@ -23,17 +22,46 @@ def index(request):
                     login(request, user)
                     return HttpResponseRedirect('/home/')
                 else:
+                    # Show the login form with an error message.
                     form = LoginForm()
                     return render(request, 'endlesstodolist/login.html', {
                         'form': form,
                         'message': "Login Error"
                     })
+            else:
+                form = LoginForm()
+                return render(request, 'endlesstodolist/login.html', {
+                    'form': form,
+                    'message': "Invalid Login"
+                })
 
         else:
+            # Show the login form as normal.
             form = LoginForm()
             return render(request, 'endlesstodolist/login.html', {
                 'form': form
             })
+    else:
+        return render(request, 'endlesstodolist/home.html')
 
 def home(request):
+    # TODO: Add live querying of the database so all tasks don't have to be loaded at once.
+    if request.user.is_authenticated():
+        tasks = Task.objects.all()
+        return render(request, 'endlesstodolist/home.html', {
+            'tasks': tasks
+        })
+    else:
+        return render(request, 'endlesstodolist/login.html', {
+            'message': "You have been logged out"
+        })
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def task(request):
+    pass
+
+def tag(request):
     pass
